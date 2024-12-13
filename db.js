deee48c55e746009f90a8c0dbd0c8497aade7ffe
Janick-aeschlimann <janick.aeschlimann@bbz-sh.ch>
2024-12-13 09:30:56 +0100

    database connections + functions

---
+++ db.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql2/promise.js");
+
+const con = mysql.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  password: "Passwort123.",
+  database: "fullstackdb",
+});
+
+async function query(query, fields) {
+  const [results] = await con.query(query, fields);
+  return results;
+}
+
+async function getAll(table) {
+  const [results] = await con.query(`SELECT * FROM ${table}`);
+  return results;
+}
+
+async function insert(table, data) {
+  let fields = Object.keys(data);
+  let values = Object.values(data);
+  await con.query(
+    `INSERT INTO \`${table}\` (\`${fields.join("`, `")}\`) VALUES (${fields
+      .map(() => "?")
+      .join(", ")});`,
+    values
+  );
+}
+
+module.exports = { query, getAll, insert };
