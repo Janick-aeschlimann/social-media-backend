5039e139c13b1f02203177a60c8cf6e01bd7e2a1
Janick-aeschlimann <janick.aeschlimann@bbz-sh.ch>
2024-12-15 13:21:25 +0100

    feat: auth middleware and socket io auth implementation

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,6 +1,25 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = (io) => {
+    io.use((socket, next) => {
+        const token = socket.handshake.auth.token;
+
+        if (!token) {
+            return next(new Error('Authentication error: No token provided'));
+        }
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                return next(new Error('Authentication error: Invalid or expired token'));
+            }
+            socket.user = decoded;
+            next();
+        });
+    });
+
+
     io.on('connection', (socket) => {
-        console.log('A user connected:', socket.id);
+        console.log('A user connected:', socket.user);
 
         // Handle events
         socket.on('message', (data) => {
